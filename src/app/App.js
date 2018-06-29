58b98416f44599de40ac978709c2c35c57017d85
andreybozhkov <bozhkov.andrey@gmail.com>
2018-06-29 12:12:32 +0300

    FIXED unique keys

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,9 @@ import '../style/App.css';
 import ContactList from './contacts.json';
 
 const showContact = contact => (
-  <div class="contact" data-id="id">
-    <span class="avatar small">&#9787;</span>
-    <span class="title">{contact.firstName} {contact.lastName}</span>
+  <div className="contact" key={contact.email} data-id="id">
+    <span className="avatar small">&#9787;</span>
+    <span className="title">{contact.firstName} {contact.lastName}</span>
   </div>
 );
 
@@ -19,30 +19,30 @@ const renderContacts = () => {
 }
 
 const App = (
-  <div class="container">
+  <div className="container">
     <header>&#9993; Contact Book</header>
       <div id="book">
         <div id="list">
           <h1>Contacts</h1>
-            <div class="content">
+            <div className="content">
               {renderContacts()};
             </div>
         </div>
         <div id="details">
           <h1>Details</h1>
-            <div class="content">
-                <div class="info">
-                    <div class="col">
-                        <span class="avatar">&#9787;</span>
+            <div className="content">
+                <div className="info">
+                    <div className="col">
+                        <span className="avatar">&#9787;</span>
                     </div>
-                    <div class="col">
-                        <span class="name">Ivan</span>
-                        <span class="name">Ivanov</span>
+                    <div className="col">
+                        <span className="name">Ivan</span>
+                        <span className="name">Ivanov</span>
                     </div>
                 </div>
-                <div class="info">
-                    <span class="info-line">&phone; 0887 123 456</span>
-                    <span class="info-line">&#9993; i.ivanov@gmail.com</span>
+                <div className="info">
+                    <span className="info-line">&phone; 0887 123 456</span>
+                    <span className="info-line">&#9993; i.ivanov@gmail.com</span>
                 </div>
             </div>
         </div>
